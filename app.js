90169de9ee803672eb978b7b547e62e1b133388a
gary909 <garywhite61@hotmail.com>
2023-06-06 12:52:09 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function solve(s){
+    //..
+}
+
+console.log(solve("code")); // "code"
+console.log(solve("CODe")); // "CODE"
+console.log(solve("COde")); // "code"
+console.log(solve("Code")); // "code"

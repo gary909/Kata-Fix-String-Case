79e7e98bb30da3b8d6e68c4208e09b72af2798c7
gary909 <garywhite61@hotmail.com>
2023-06-06 12:58:12 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 function solve(s){
-    //..
+    // Solve how many upper & lower cases exist
+    var numUpper = s.length - s.replace(/[A-Z]/g, '').length;  
+    var numLower = s.length - s.replace(/[a-z]/g, '').length;  
+    if(numLower > numUpper || numLower == numUpper){
+        return s.toLowerCase();
+    } else {
+        return s.toUpperCase();
+    }
 }
 
 console.log(solve("code")); // "code"
